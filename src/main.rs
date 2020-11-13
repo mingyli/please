ecab6ab7ea83a2653efcb377e8b59ff326aa496f
ming li <mingyli34@gmail.com>
2020-11-12 17:05:00 -0800

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use std::io::{self, Read, Write};
 use std::process::{Command, Stdio};
 use std::sync::mpsc;
 
+fn proxy_output(sender: mpsc::Sender<u8>, output: &mut dyn Read) -> io::Result<()> {
+    for byte in output.bytes() {
+        sender.send(byte?).unwrap();
+    }
+    Ok(())
+}
+
+fn forward_output(receiver: mpsc::Receiver<u8>, mut out: &mut dyn Write) -> io::Result<()> {
+    for byte in receiver {
+        out.write_all(&[byte])?;
+        if byte as char == '?' {
+            write!(&mut out, "🥺👉👈")?;
+        }
+        out.flush()?;
+    }
+    Ok(())
+}
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
 
@@ -13,34 +31,18 @@ fn main() -> io::Result<()> {
 
     let (stdout_sender, stdout_receiver) = mpsc::channel();
     let (stderr_sender, stderr_receiver) = mpsc::channel();
-    let stdout = child.stdout.take().unwrap();
-    let stderr = child.stderr.take().unwrap();
-    std::thread::spawn(move || -> io::Result<()> {
-        for byte in stdout.bytes() {
-            stdout_sender.send(byte?).unwrap();
-        }
-        Ok(())
-    });
-    std::thread::spawn(move || -> io::Result<()> {
-        for byte in stderr.bytes() {
-            stderr_sender.send(byte?).unwrap();
-        }
-        Ok(())
-    });
+    let mut stdout = child.stdout.take().unwrap();
+    let mut stderr = child.stderr.take().unwrap();
 
-    fn forward_output(receiver: mpsc::Receiver<u8>, mut out: &mut dyn Write) -> io::Result<()> {
-        for byte in receiver {
-            out.write_all(&[byte])?;
-            if byte as char == '?' {
-                write!(&mut out, "🥺👉👈")?;
-            }
-            out.flush()?;
-        }
-        Ok(())
-    }
-
-    std::thread::spawn(move || forward_output(stdout_receiver, &mut io::stdout()));
-    forward_output(stderr_receiver, &mut io::stderr())?;
-
+    let proxy_stdout = std::thread::spawn(move || proxy_output(stdout_sender, &mut stdout));
+    let proxy_stderr = std::thread::spawn(move || proxy_output(stderr_sender, &mut stderr));
+    let forward_stdout =
+        std::thread::spawn(move || forward_output(stdout_receiver, &mut io::stdout()));
+    let forward_stderr =
+        std::thread::spawn(move || forward_output(stderr_receiver, &mut io::stderr()));
+    proxy_stdout.join().unwrap()?;
+    proxy_stderr.join().unwrap()?;
+    forward_stdout.join().unwrap()?;
+    forward_stderr.join().unwrap()?;
     Ok(())
 }
